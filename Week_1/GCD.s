16cf64b8bdb7f878655e3bac758b5dca77751c18
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2024-02-15 20:23:01 +0530

    Update GCD.s

--- Week_1/GCD.s
+++ Week_1/GCD.s
@@ -1,5 +1,7 @@
+;Program to find the GCD of two numbers
 mov r0,#18
 mov r1,#72
+
 loop:cmp r0,r1
 beq exit
 blt loop1
@@ -8,4 +10,5 @@ b loop
 loop1:sub r1,r1,r0
 b loop
 exit:mov r3,r0
+
 swi 0x11
