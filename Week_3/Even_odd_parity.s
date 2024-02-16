21dd137bfa1bb2fe3a9d30074fb3f301099fac89
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2024-02-16 07:18:11 +0530

    Add files via upload

---
+++ Week_3/Even_odd_parity.s
@@ -0,0 +1,27 @@
+.data
+    A: .word 0
+    B: .word 1
+.text
+    mov R0, #3
+    mov R1, #1
+    mov R3, #2
+loop:
+eor R0, R0, R0, LSR R1
+mul R4, R1, R3
+mov R1, R4
+cmp R1, #32
+beq exit
+B loop
+
+exit:
+and R0,R0,#1
+cmp R0, #1
+beq oddparity
+ldr R5,=A
+ldr r5,[r5]
+SWI 0x011
+
+oddparity:
+ldr R5,=B
+ldr r5,[r5]
+SWI 0x011
